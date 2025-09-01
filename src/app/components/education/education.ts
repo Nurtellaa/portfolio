95d68d8784071d6c4d77aa6c7f569cc5f2f1b783
nur-dam <nbohigasg01@educantabria.es>
2025-09-01 15:03:56 +0200

    ADD: education

--- src/app/components/education/education.ts
+++ src/app/components/education/education.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-education',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.html',
   styleUrl: './education.scss'
 })
 export class Education {
+  isOpen: boolean[] = [false, false];
 
+  toggleAccordion(index: number) {
+    this.isOpen[index] = !this.isOpen[index];
+  }
 }
